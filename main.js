86de5cb35b64bda607ca5e1415c91ad9a221bd2e
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-01-26 18:42:35 +1300

    Add road with lane markings

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ const canvas = document.getElementById('myCanvas')
 canvas.width = 200
 
 const ctx = canvas.getContext('2d')
-const car = new Car(100, 100, 30, 50)
+
+const road = new Road(canvas.width / 2, canvas.width * 0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50)
 
 animate()
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  canvas.height = window.innerHeight
   car.update()
+
+  canvas.height = window.innerHeight
+
+  ctx.save()
+  ctx.translate(0, -car.y + canvas.height * 0.7)
+
+  road.draw(ctx)
   car.draw(ctx)
+
+  ctx.restore()
   requestAnimationFrame(animate)
 }
